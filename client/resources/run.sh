a96563d90602806a9caa3886e0a5c7163dbfee9d
Brady Wied <brady@bswtechconsulting.com>
2017-11-11 21:14:44 -0700

    Complete naming change

--- client/resources/run.sh
+++ client/resources/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOCKER_IP=`getent hosts server | awk '{ print $1 }'`
+DOCKER_IP=`getent hosts kdc | awk '{ print $1 }'`
 echo "Setting up resolution of kerberos.example.com to $DOCKER_IP"
 echo "$DOCKER_IP kerberos.example.com" >> /etc/hosts
 echo -e "thePassword" | kinit brady/admin@EXAMPLE.COM

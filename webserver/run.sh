755259a89fbe8c5c4570ea5ebbc620175a3579a7
Brady Wied <brady@bswtechconsulting.com>
2017-11-12 00:02:42 -0700

    Set FQDN

--- webserver/run.sh
+++ webserver/run.sh
@@ -9,7 +9,7 @@ echo "Pulling down keys from KDC into webserver keytab..."
 KEYTAB_LOCATION="/etc/krb5.keytab"
 HOSTNAME_TO_USE="webserver"
 echo "Adjusting hostname to match principal ($HOSTNAME_TO_USE)"
-hostname $HOSTNAME_TO_USE
+hostname "$HOSTNAME_TO_USE.example.com"
 echo -e "thePassword" | kadmin -p brady/admin@EXAMPLE.COM -q "ktadd -k $KEYTAB_LOCATION HTTP/$HOSTNAME_TO_USE@EXAMPLE.COM"
 echo "Done pulling down keys from KDC into webserver keytab, launching web server..."
 KEYTAB_LOCATION=$KEYTAB_LOCATION rackup -o 0.0.0.0 /usr/local/bin/config.ru

386c4bd31ac48dc24ac4bd00d013c09ec714e724
Brady Wied <brady@bswtechconsulting.com>
2017-11-11 21:25:45 -0700

    Show what's going on

--- client/resources/run.sh
+++ client/resources/run.sh
@@ -3,5 +3,7 @@
 DOCKER_IP=`getent hosts kdc | awk '{ print $1 }'`
 echo "Setting up resolution of kerberos.example.com to $DOCKER_IP"
 echo "$DOCKER_IP kerberos.example.com" >> /etc/hosts
+echo "Getting a kerberos ticket for brady/admin@EXAMPLE.COM"
 echo -e "thePassword" | kinit brady/admin@EXAMPLE.COM
+echo "kinit complete"
 klist
